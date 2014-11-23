26287149652d5964c6c0b1fac3ace6fda4486d9a
philiprobey <philipmrobeymn@gmail.com>
2014-11-23 02:52:11 -0600

    added to the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function takes a matrix as an argument and stores it with an initial null matrix
+## along with creating a list of 4 functions to perform on the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## this functions gets the inverse of the matrix x.  This is matrix m, which initially is set
+## to null. Thus on the first calling of chacheSolve() it passes the if statement and uses 
+## the code below that determine the mean and assign the inverse of x to the inverted matrix.
+## on every subsequent calling of cacheSolve() the inverse of x is not null so it goes
+## through the if statment and retrieves the inverted matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
